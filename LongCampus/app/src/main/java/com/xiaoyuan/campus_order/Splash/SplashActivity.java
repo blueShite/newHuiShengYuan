a1e55535ef22261a0a3b6a975282fca234efc196
龙恒宇 <longhengyu@longhengyudeMac-mini.local>
2017-07-20 18:35:10 +0800

    添加权限提示

--- LongCampus/app/src/main/java/com/xiaoyuan/campus_order/Splash/SplashActivity.java
+++ LongCampus/app/src/main/java/com/xiaoyuan/campus_order/Splash/SplashActivity.java
@@ -1,9 +1,13 @@
 package com.xiaoyuan.campus_order.Splash;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.widget.ImageView;
 
 import com.xiaoyuan.campus_order.Login.Bean.LoginBean;
@@ -12,9 +16,14 @@ import com.xiaoyuan.campus_order.Manage.LoginManage;
 import com.xiaoyuan.campus_order.R;
 import com.xiaoyuan.campus_order.Tab.TabActivity;
 import com.squareup.picasso.Picasso;
+import com.xiaoyuan.campus_order.Tools.ActivityCollector;
 
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.RuntimePermissions;
 import qiu.niorgai.StatusBarCompat;
 
+@RuntimePermissions
 public class SplashActivity extends Activity {
 
     private ImageView mImageView;
@@ -27,13 +36,7 @@ public class SplashActivity extends Activity {
         StatusBarCompat.translucentStatusBar(SplashActivity.this, true);
         mImageView = (ImageView)findViewById(R.id.image_splash);
         Picasso.with(SplashActivity.this).load(R.drawable.splash).fit().centerCrop().into(mImageView);
-        new Handler().postDelayed(new Runnable() {
-
-            @Override
-            public void run() {
-                enterHomeActivity();
-            }
-        }, 2000);
+        SplashActivityPermissionsDispatcher.onPermissionsAllGrantedWithCheck(this);
     }
 
     private void enterHomeActivity(){
@@ -50,5 +53,52 @@ public class SplashActivity extends Activity {
         finish();
     }
 
+    @NeedsPermission({Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.CAMERA,
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.READ_EXTERNAL_STORAGE})
+    public void onPermissionsAllGranted() {
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                enterHomeActivity();
+            }
+        }, 2000);
+    }
+
+    @OnPermissionDenied({Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.CAMERA,
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.READ_EXTERNAL_STORAGE})
+    public void showPermissionsDenied(){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(SplashActivity.this);
+        builder.setTitle("权限提示");
+        builder.setMessage("我们需要获取一些基本权限，否则您将无法正常使用惠生园app");
+        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface anInterface, int i) {
+                recreate();
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface anInterface, int i) {
+                System.exit(0);
+            }
+        });
+        builder.show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        SplashActivityPermissionsDispatcher.onRequestPermissionsResult(this,requestCode,grantResults);
+    }
 
 }
